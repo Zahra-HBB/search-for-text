5d82ae79ed05429578dda5fea821671558060cb8
Zahra-HBB <zahrahabibi63@gmail.com>
2021-05-16 19:31:49 +1000

    refactoring search to move read file into main

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ from search import search
 
 
 def main():
-    search()
+    text_file = open("./data/usa/usa.txt", "r")
+    english_words = text_file.readlines()
+
+    search_term = input("Please enter a string: ")
+
+    string_count = search(english_words, search_term)
+
+    if string_count["str_count"] == 0:
+        print("No occurance found!")
+    else:
+        print("Total number of '{}' occurance is: {} \nThe longest word includes input is: {}".format(
+            search_term, string_count["str_count"], string_count["max_text"]))
 
 
 if __name__ == '__main__':
--- search.py
+++ search.py
@@ -1,28 +1,15 @@
-text_file = open("./data/usa/usa.txt", "rt")
-english_words = text_file.readlines()
-
 string_count = {
     "str_count": 0,
     "max_text": ''
 }
 
 
-input = input("Please enter a string: ")
+def search(english_words, search_term):
 
+    for line in english_words:
+        if search_term in line:
+            string_count["str_count"] = string_count["str_count"] + 1
 
-def search():
-    if input in line:
-        string_count["str_count"] = string_count["str_count"] + 1
-
-        if len(string_count["max_text"]) <= len(line):
-            string_count["max_text"] = line
-
-
-for line in english_words:
-    search()
-
-if string_count["str_count"] == 0:
-    print("No occurance found!")
-else:
-    print("Total number of '{}' occurance is: {} \nThe longest word includes input is: {}".format(
-        input, string_count["str_count"], string_count["max_text"]))
+            if len(string_count["max_text"]) <= len(line):
+                string_count["max_text"] = line
+    return string_count
